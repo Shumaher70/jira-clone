044ae052aab6ae67f111a8d2ac4b34bb995dde88
Oleg Vrabiy <olegvraby@gmail.com>
2024-11-22 17:52:07 +0200

    update:auth/server/route.ts: Additional method for getting current user has been added

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -4,11 +4,17 @@ import { zValidator } from "@hono/zod-validator";
 import { deleteCookie, setCookie } from "hono/cookie";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { AUTH_COOKIE } from "../constants";
 import { loginSchema, registerSchema } from "../schemas";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -44,8 +50,12 @@ const app = new Hono()
     return c.json({ success: true });
   })
 
-  .post("/logout", async (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
