44acb53ce3be465745ba148f771a66cca8447345
Oleg Vrabiy <olegvraby@gmail.com>
2024-12-27 18:45:34 +0200

    create:create-task-form-wrapper.tsx

---
+++ src/features/tasks/components/create-task-form-wrapper.tsx
@@ -0,0 +1,56 @@
+import { Loader } from "lucide-react";
+
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspaceId";
+
+import { CreateTaskForm } from "./create-task-form";
+import { Card, CardContent } from "@/components/ui/card";
+
+interface CreateTaskFormWrapperProps {
+  onCancel: () => void;
+}
+
+export const CreateTaskFormWrapper = ({
+  onCancel,
+}: CreateTaskFormWrapperProps) => {
+  const workspaceId = useWorkspaceId(); // Assuming useWorkspaceId hook is available
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const projectOptions = projects?.documents.map((project) => ({
+    id: project.$id,
+    name: project.name,
+    imageUrl: project.imageUrl,
+  }));
+  const memberOptions = members?.documents.map((member) => ({
+    id: member.$id,
+    name: member.name,
+  }));
+
+  const isLoading = isLoadingMembers || isLoadingProjects;
+
+  if (isLoading) {
+    return (
+      <Card className="h-[714px] w-full border-none shadow-none">
+        <CardContent className="flex h-full items-center justify-center">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <div>
+      <CreateTaskForm
+        onCancel={onCancel}
+        projectOptions={projectOptions ?? []}
+        memberOptions={memberOptions ?? []}
+      />
+    </div>
+  );
+};
