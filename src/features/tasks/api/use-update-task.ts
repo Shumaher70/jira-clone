593c52b33bc74320e79c2409db162bfb665d498c
Oleg Vrabiy <olegvraby@gmail.com>
2025-01-21 02:18:25 +0200

    create:use-update-task.ts

---
+++ src/features/tasks/api/use-update-task.ts
@@ -0,0 +1,46 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.tasks)[":taskId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.tasks)[":taskId"]["$patch"]
+>;
+
+export const useUpdateTask = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json, param }) => {
+      const response = await client.api.tasks[":taskId"]["$patch"]({
+        json,
+        param,
+      });
+
+      if (!response.ok) {
+        throw new Error("filed to update tasks");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Tasks updated successfully!");
+
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      queryClient.invalidateQueries({ queryKey: ["task", data.$id] });
+    },
+    onError: () => {
+      toast.error("Failed to update tasks. Please try again.");
+    },
+  });
+
+  return mutation;
+};
